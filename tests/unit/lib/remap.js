a82069b0384cfaf6e95af0287531e153af59e667
Kitson Kelly <me@kitsonkelly.com>
2015-08-21 23:00:01 +0100

    Add testing for inline source maps

--- tests/unit/lib/remap.js
+++ tests/unit/lib/remap.js
@@ -19,6 +19,20 @@ define([
 			assert.strictEqual(Object.keys(map.statementMap).length, 28, 'Map should have 28 statements');
 			assert.strictEqual(Object.keys(map.fnMap).length, 6, 'Map should have 6 functions');
 			assert.strictEqual(Object.keys(map.branchMap).length, 6, 'Map should have 6 branches');
+		},
+
+		'base64 source map': function () {
+			var coverage = remap({ sources: [ 'tests/unit/support/inline-coverage.json' ] });
+			assert.instanceOf(coverage, Collector, 'Return values should be instance of Collector');
+			assert(coverage.store.map['tests/unit/support/basic.ts'],
+				'The Collector should have a remapped key');
+			assert.strictEqual(Object.keys(coverage.store.map).length, 1,
+				'Collector should only have one map');
+			var map = JSON.parse(coverage.store.map['tests/unit/support/basic.ts']);
+			assert.strictEqual(map.path, 'tests/unit/support/basic.ts');
+			assert.strictEqual(Object.keys(map.statementMap).length, 28, 'Map should have 28 statements');
+			assert.strictEqual(Object.keys(map.fnMap).length, 6, 'Map should have 6 functions');
+			assert.strictEqual(Object.keys(map.branchMap).length, 6, 'Map should have 6 branches');
 		}
 	});
 });
