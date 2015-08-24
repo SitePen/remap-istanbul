1e3d13b576cbd08f89ccc9abab6a6b8cd5597e0d
Kitson Kelly <me@kitsonkelly.com>
2015-08-24 17:07:26 +0100

    Allow passing of a string in main

--- main.js
+++ main.js
@@ -3,7 +3,19 @@
 var remap = require('./lib/remap');
 var writeReport = require('./lib/writeReport');
 
+/**
+ * The basic API for utilising remap-istanbul
+ * @param  {Array|string} sources The sources that could be consumed and remapped.
+ *                                For muliple sources to be combined together, provide
+ *                                an array of strings.
+ * @param  {Object} reports An object where each key is the report type required and the value
+ *                          is the destination for the report.
+ * @return {Promise}         A promise that will resolve when all the reports are written.
+ */
 module.exports = function (sources, reports) {
+	if (typeof sources === 'string') {
+		sources = [ sources ];
+	}
 	var collector = remap({ sources: sources });
 
 	var p = Object.keys(reports).map(function (reportType) {
--- tests/unit/main.js
+++ tests/unit/main.js
@@ -6,6 +6,7 @@ define([
 ], function (registerSuite, assert, fs, main) {
 	registerSuite({
 		name: 'main',
+
 		'interface': function () {
 			return main([ 'tests/unit/support/coverage.json' ], {
 				'lcovonly': 'tmp/main.lcov.info',
@@ -16,6 +17,17 @@ define([
 				assert.include(lcovonly, 'SF:tests/unit/support/basic.ts',
 					'should have the mapped file name');
 				var json = JSON.parse(fs.readFileSync('tmp/main.json', { encoding: 'utf8' }));
+				assert(json, 'should have returned content');
+				assert(json['tests/unit/support/basic.ts'],
+					'should have key named after mapped file');
+			});
+		},
+
+		'string argument': function () {
+			return main('tests/unit/support/coverage.json', {
+				'json': 'tmp/main-string.json'
+			}).then(function () {
+				var json = JSON.parse(fs.readFileSync('tmp/main-string.json', { encoding: 'utf8' }));
 				assert(json, 'should have returned content');
 				assert(json['tests/unit/support/basic.ts'],
 					'should have key named after mapped file');
