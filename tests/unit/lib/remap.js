a59ce4f95abd954304de06fc381f94a378d0bcfe
Kitson Kelly <me@kitsonkelly.com>
2015-08-27 17:43:56 +0100

    Refactor remap

--- tests/unit/lib/remap.js
+++ tests/unit/lib/remap.js
@@ -2,13 +2,14 @@ define([
 	'intern!object',
 	'intern/chai!assert',
 	'../../../lib/node!istanbul/lib/collector',
+	'../../../lib/loadCoverage',
 	'../../../lib/remap'
-], function (registerSuite, assert, Collector, remap) {
+], function (registerSuite, assert, Collector, loadCoverage, remap) {
 	registerSuite({
 		name: 'remap-istanbul/lib/remap',
 
 		'remapping': function () {
-			var coverage = remap({ sources: [ 'tests/unit/support/coverage.json' ] });
+			var coverage = remap(loadCoverage('tests/unit/support/coverage.json'));
 			assert.instanceOf(coverage, Collector, 'Return values should be instance of Collector');
 			assert(coverage.store.map['tests/unit/support/basic.ts'],
 				'The Collector should have a remapped key');
@@ -22,7 +23,7 @@ define([
 		},
 
 		'base64 source map': function () {
-			var coverage = remap({ sources: [ 'tests/unit/support/inline-coverage.json' ] });
+			var coverage = remap(loadCoverage('tests/unit/support/inline-coverage.json'));
 			assert.instanceOf(coverage, Collector, 'Return values should be instance of Collector');
 			assert(coverage.store.map['tests/unit/support/basic.ts'],
 				'The Collector should have a remapped key');
@@ -33,36 +34,6 @@ define([
 			assert.strictEqual(Object.keys(map.statementMap).length, 28, 'Map should have 28 statements');
 			assert.strictEqual(Object.keys(map.fnMap).length, 6, 'Map should have 6 functions');
 			assert.strictEqual(Object.keys(map.branchMap).length, 6, 'Map should have 6 branches');
-		},
-
-		'invalid source path': function () {
-			var warnStack = [];
-			function warn() {
-				warnStack.push(arguments);
-			}
-
-			remap({
-				sources: [ 'badpath.json' ],
-				warn: warn
-			});
-
-			assert.instanceOf(warnStack[0][0], Error);
-			assert.strictEqual(warnStack[0][0].message, 'Cannot find file: "badpath.json"');
-		},
-
-		'empty sources': function () {
-			var warnStack = [];
-			function warn() {
-				warnStack.push(arguments);
-			}
-
-			remap({
-				sources: [],
-				warn: warn
-			});
-
-			assert.instanceOf(warnStack[0][0], SyntaxError);
-			assert.strictEqual(warnStack[0][0].message, 'No coverage files supplied!');
 		},
 
 		'empty options': function () {
