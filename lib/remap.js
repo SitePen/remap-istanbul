zxbodya/istanbul-sourcemap-report

smaller modules, refactoring

lib/remap.js
@@ -8,116 +8,14 @@ const { SourceMapConsumer } = require('source-map/lib/source-map-consumer');
 
 /* global WeakMap */
 
+const updateFunctions = require('./updateFunctions');
+const updateStatements = require('./updateStatements');
+const updateBranches = require('./updateBranches');
 
 const sourceMapRegEx = /(?:\/{2}[#@]{1,2}|\/\*)\s+sourceMappingURL\s*=\s*(data:(?:[^;]+;)+base64,)?(\S+)/;
 
-const metaInfo = new WeakMap();
 
-/**
- * Generate a coverage object that will be filled with the remapped data
- * @param  {Object} srcCoverage The coverage object to be populated
- * @param  {string} filename    The name of the file that is being remapped
- * @return {Object}             An object that provides the actual data and
- *                              its shadow data used for reference.
- */
-function getSourceCoverage(srcCoverage, filename) {
-  let data = srcCoverage[filename];
-  if (!data) {
-    data = srcCoverage[filename] = {
-      path: filename,
-      statementMap: {},
-      fnMap: {},
-      branchMap: {},
-      s: {},
-      b: {},
-      f: {},
-    };
-    metaInfo.set(data, {
-      indexes: {},
-      lastIndex: {
-        s: 0,
-        b: 0,
-        f: 0,
-      },
-    });
-  }
-
-  return {
-    data,
-    meta: metaInfo.get(data),
-  };
-}
-
-/**
- * A function that determines the original position for a given location
- * @param  {SourceMapConsumer} sourceMap        The source map
- * @param  {string}            sourceMapDir     The directory where the original is located
- * @param  {Object}            location         The original location Object
- * @param  {Boolean}           useAbsolutePaths If `true`, don't resolve the path relative to the `cwd`
- * @param  {Object}            inlineSourceMap  If `true`, don't try to resolve the source path, just copy it
- * @return {Object}                             The remapped location Object
- */
-function getMapping(sourceMap, sourceMapDir, location, useAbsolutePaths, inlineSourceMap) {
-  /* jshint maxcomplexity: 15 */
-
-  /* istanbul ignore if: edge case too hard to test for with babel malformation */
-  if (location.start.line < 1 || location.start.column < 0) {
-    return null;
-  }
-  /* istanbul ignore if: edge case too hard to test for with babel malformation */
-  if (location.end.line < 1 || location.end.column < 0) {
-    return null;
-  }
-
-  const start = sourceMap.originalPositionFor(location.start);
-  let end = sourceMap.originalPositionFor(location.end);
-  let resolvedSource;
-
-  /* istanbul ignore if: edge case too hard to test for */
-  if (!start || !end) {
-    return null;
-  }
-  if (!start.source || !end.source || start.source !== end.source) {
-    return null;
-  }
-  /* istanbul ignore if: edge case too hard to test for */
-  if (start.line === null || start.column === null) {
-    return null;
-  }
-  /* istanbul ignore if: edge case too hard to test for */
-  if (end.line === null || end.column === null) {
-    return null;
-  }
-
-  if (start.line === end.line && start.column === end.column) {
-    end = sourceMap.originalPositionFor({
-      line: location.end.line,
-      column: location.end.column,
-      bias: 2,
-    });
-    end.column = end.column - 1;
-  }
-
-  resolvedSource = start.source in inlineSourceMap ? start.source : path.resolve(sourceMapDir, start.source);
-  if (!useAbsolutePaths && !(start.source in inlineSourceMap)) {
-    resolvedSource = path.relative(process.cwd(), resolvedSource);
-  }
-  return {
-    source: resolvedSource,
-    loc: {
-      start: {
-        line: start.line,
-        column: start.column,
-      },
-      end: {
-        line: end.line,
-        column: end.column,
-      },
-      skip: location.skip,
-    },
-  };
-}
-
+const getMapping = require('./getMapping');
 /**
  * Remaps coverage data based on the source maps it discovers in the
  * covered files and returns a coverage Collector that contains the remappped
@@ -138,19 +36,15 @@ function getMapping(sourceMap, sourceMapDir, location, useAbsolutePaths, inlineS
  *                              warn?     - a function that logs warnings
  * @return {istanbul/lib/_collector}         The remapped collector
  */
-module.exports = function remap(coverage, options = {}) {
+function remap(coverage, options = {}) {
   const warn = options.warn || console.warn;
 
   let exclude;
   if (options.exclude) {
     if (typeof options.exclude === 'string') {
-      exclude = function (fileName) {
-        return fileName.indexOf(options.exclude) > -1;
-      };
+      exclude = (fileName) => fileName.indexOf(options.exclude) > -1;
     } else {
-      exclude = function (fileName) {
-        return fileName.match(options.exclude);
-      };
+      exclude = (fileName) => fileName.match(options.exclude);
     }
   }
 
@@ -175,11 +69,47 @@ module.exports = function remap(coverage, options = {}) {
       return fs.readFileSync(filePath);
     };
 
-  let srcCoverage = {};
 
   if (!Array.isArray(coverage)) {
     coverage = [coverage];
   }
+
+  let srcCoverage = {};
+  const metaInfo = new WeakMap();
+
+  /**
+   * Generate a coverage object that will be filled with the remapped data
+   * @param  {string} filename    The name of the file that is being remapped
+   * @return {Object}             An object that provides the actual data and
+   *                              its shadow data used for reference.
+   */
+  const getSourceCoverage = (filename) => {
+    let data = srcCoverage[filename];
+    if (!data) {
+      data = srcCoverage[filename] = {
+        path: filename,
+        statementMap: {},
+        fnMap: {},
+        branchMap: {},
+        s: {},
+        b: {},
+        f: {},
+      };
+      metaInfo.set(data, {
+        indexes: {},
+        lastIndex: {
+          s: 0,
+          b: 0,
+          f: 0,
+        },
+      });
+    }
+
+    return {
+      data,
+      meta: metaInfo.get(data),
+    };
+  };
 
   coverage.forEach((item) => {
     Object.keys(item).forEach((filePath) => {
@@ -187,6 +117,7 @@ module.exports = function remap(coverage, options = {}) {
         warn(`Excluding: "${filePath}"`);
         return;
       }
+
       const fileCoverage = item[filePath];
       /* coverage.json can sometimes include the code inline */
       let codeIsArray = true;
@@ -231,131 +162,35 @@ module.exports = function remap(coverage, options = {}) {
       if (sourceMap.sourcesContent) {
         sourceMap.sourcesContent.forEach((source, idx) => {
           inlineSourceMap[sourceMap.sources[idx]] = true;
-          getSourceCoverage(srcCoverage, sourceMap.sources[idx]).data.code = codeIsArray ? source.split('\n') : source;
+          getSourceCoverage(sourceMap.sources[idx]).data.code = codeIsArray ? source.split('\n') : source;
           if (sourceStore) {
             sourceStore.set(sourceMap.sources[idx], source);
           }
         });
       }
 
-      Object.keys(fileCoverage.fnMap).forEach((index) => {
-        const genItem = fileCoverage.fnMap[index];
-        const mapping = getMapping(sourceMap, sourceMapDir, genItem.loc, useAbsolutePaths, inlineSourceMap);
+      function resolvePath(source) {
+        let resolvedSource = source in inlineSourceMap
+          ? source
+          : path.resolve(sourceMapDir, source);
 
-        if (!mapping) {
-          return;
+        if (!useAbsolutePaths && !(source in inlineSourceMap)) {
+          resolvedSource = path.relative(process.cwd(), resolvedSource);
         }
+        return resolvedSource;
+      }
 
-        const hits = fileCoverage.f[index];
-        const covInfo = getSourceCoverage(srcCoverage, mapping.source);
-        const data = covInfo.data;
-        const meta = covInfo.meta;
-        const srcItem = {
-          name: genItem.name,
-          line: mapping.loc.start.line,
-          loc: mapping.loc,
-        };
-        if (genItem.skip) {
-          srcItem.skip = genItem.skip;
-        }
-        const key = [
-          'f',
-          srcItem.loc.start.line, srcItem.loc.start.column,
-          srcItem.loc.end.line, srcItem.loc.end.column,
-        ].join(':');
+      const getMappingResolved = (location) => {
+        const mapping = getMapping(sourceMap, location);
+        if (!mapping) return null;
 
-        let fnIndex = meta.indexes[key];
-        if (!fnIndex) {
-          fnIndex = ++meta.lastIndex.f;
-          meta.indexes[key] = fnIndex;
-          data.fnMap[fnIndex] = srcItem;
-        }
-        data.f[fnIndex] = data.f[fnIndex] || 0;
-        data.f[fnIndex] += hits;
-      });
+        return Object.assign(mapping, { source: resolvePath(mapping.source) });
+      };
 
-      Object.keys(fileCoverage.statementMap).forEach((index) => {
-        const genItem = fileCoverage.statementMap[index];
 
-        const mapping = getMapping(sourceMap, sourceMapDir, genItem, useAbsolutePaths, inlineSourceMap);
-
-        if (!mapping) {
-          return;
-        }
-
-        const hits = fileCoverage.s[index];
-        const covInfo = getSourceCoverage(srcCoverage, mapping.source);
-        const data = covInfo.data;
-        const meta = covInfo.meta;
-
-        const key = [
-          's',
-          mapping.loc.start.line, mapping.loc.start.column,
-          mapping.loc.end.line, mapping.loc.end.column,
-        ].join(':');
-
-        let stIndex = meta.indexes[key];
-        if (!stIndex) {
-          stIndex = ++meta.lastIndex.s;
-          meta.indexes[key] = stIndex;
-          data.statementMap[stIndex] = mapping.loc;
-        }
-        data.s[stIndex] = data.s[stIndex] || 0;
-        data.s[stIndex] += hits;
-      });
-
-      Object.keys(fileCoverage.branchMap).forEach((index) => {
-        const genItem = fileCoverage.branchMap[index];
-        const locations = [];
-        let source;
-        let key = ['b'];
-
-        for (let i = 0; i < genItem.locations.length; ++i) {
-          const mapping = getMapping(sourceMap, sourceMapDir, genItem.locations[i], useAbsolutePaths, inlineSourceMap);
-          if (!mapping) {
-            return;
-          }
-          /* istanbul ignore else: edge case too hard to test for */
-          if (!source) {
-            source = mapping.source;
-          } else {
-            if (source !== mapping.source) {
-              return;
-            }
-          }
-          locations.push(mapping.loc);
-          key.push(
-            mapping.loc.start.line, mapping.loc.start.column,
-            mapping.loc.end.line, mapping.loc.end.line
-          );
-        }
-
-        key = key.join(':');
-
-        const hits = fileCoverage.b[index];
-        const covInfo = getSourceCoverage(srcCoverage, source);
-        const data = covInfo.data;
-        const meta = covInfo.meta;
-
-        let brIndex = meta.indexes[key];
-        if (!brIndex) {
-          brIndex = ++meta.lastIndex.b;
-          meta.indexes[key] = brIndex;
-          data.branchMap[brIndex] = {
-            line: locations[0].start.line,
-            type: genItem.type,
-            locations,
-          };
-        }
-
-        if (!data.b[brIndex]) {
-          data.b[brIndex] = locations.map(() => 0);
-        }
-
-        for (let i = 0; i < hits.length; ++i) {
-          data.b[brIndex][i] += hits[i];
-        }
-      });
+      updateFunctions(fileCoverage, getMappingResolved, getSourceCoverage);
+      updateStatements(fileCoverage, getMappingResolved, getSourceCoverage);
+      updateBranches(fileCoverage, getMappingResolved, getSourceCoverage);
     });
   });
 
@@ -374,4 +209,6 @@ module.exports = function remap(coverage, options = {}) {
   collector.getFinalCoverage();
 
   return collector;
-};
+}
+
+module.exports = remap;

SitePen/remap-istanbul

lib/remap.js
@@ -8,6 +8,9 @@ const { SourceMapConsumer } = require('source-map/lib/source-map-consumer');
 
 /* global WeakMap */
 
+const updateFunctions = require('./updateFunctions');
+const updateStatements = require('./updateStatements');
+const updateBranches = require('./updateBranches');
 
 const sourceMapRegEx = /(?:\/{2}[#@]{1,2}|\/\*)\s+sourceMappingURL\s*=\s*(data:(?:[^;]+;)+base64,)?(\S+)/;
 
@@ -33,112 +36,7 @@ const MaybeWeakMap = typeof WeakMap === 'function' ? WeakMap : function () {
   };
 };
 
-const metaInfo = new MaybeWeakMap();
-/**
- * Generate a coverage object that will be filled with the remapped data
- * @param  {Object} srcCoverage The coverage object to be populated
- * @param  {string} filename    The name of the file that is being remapped
- * @return {Object}             An object that provides the actual data and
- *                              its shadow data used for reference.
- */
-function getSourceCoverage(srcCoverage, filename) {
-  let data = srcCoverage[filename];
-  if (!data) {
-    data = srcCoverage[filename] = {
-      path: filename,
-      statementMap: {},
-      fnMap: {},
-      branchMap: {},
-      s: {},
-      b: {},
-      f: {},
-    };
-    metaInfo.set(data, {
-      indexes: {},
-      lastIndex: {
-        s: 0,
-        b: 0,
-        f: 0,
-      },
-    });
-  }
-
-  return {
-    data,
-    meta: metaInfo.get(data),
-  };
-}
-
-/**
- * A function that determines the original position for a given location
- * @param  {SourceMapConsumer} sourceMap        The source map
- * @param  {string}            sourceMapDir     The directory where the original is located
- * @param  {Object}            location         The original location Object
- * @param  {Boolean}           useAbsolutePaths If `true`, don't resolve the path relative to the `cwd`
- * @param  {Object}            inlineSourceMap  If `true`, don't try to resolve the source path, just copy it
- * @return {Object}                             The remapped location Object
- */
-function getMapping(sourceMap, sourceMapDir, location, useAbsolutePaths, inlineSourceMap) {
-  /* jshint maxcomplexity: 15 */
-
-  /* istanbul ignore if: edge case too hard to test for with babel malformation */
-  if (location.start.line < 1 || location.start.column < 0) {
-    return null;
-  }
-  /* istanbul ignore if: edge case too hard to test for with babel malformation */
-  if (location.end.line < 1 || location.end.column < 0) {
-    return null;
-  }
-
-  const start = sourceMap.originalPositionFor(location.start);
-  let end = sourceMap.originalPositionFor(location.end);
-  let resolvedSource;
-
-  /* istanbul ignore if: edge case too hard to test for */
-  if (!start || !end) {
-    return null;
-  }
-  if (!start.source || !end.source || start.source !== end.source) {
-    return null;
-  }
-  /* istanbul ignore if: edge case too hard to test for */
-  if (start.line === null || start.column === null) {
-    return null;
-  }
-  /* istanbul ignore if: edge case too hard to test for */
-  if (end.line === null || end.column === null) {
-    return null;
-  }
-
-  if (start.line === end.line && start.column === end.column) {
-    end = sourceMap.originalPositionFor({
-      line: location.end.line,
-      column: location.end.column,
-      bias: 2,
-    });
-    end.column = end.column - 1;
-  }
-
-  resolvedSource = start.source in inlineSourceMap ? start.source : path.resolve(sourceMapDir, start.source);
-  if (!useAbsolutePaths && !(start.source in inlineSourceMap)) {
-    resolvedSource = path.relative(process.cwd(), resolvedSource);
-  }
-  return {
-    source: resolvedSource,
-    loc: {
-      start: {
-        line: start.line,
-        column: start.column,
-      },
-      end: {
-        line: end.line,
-        column: end.column,
-      },
-      skip: location.skip,
-    },
-  };
-}
-
+const getMapping = require('./getMapping');
 /**
  * Remaps coverage data based on the source maps it discovers in the
  * covered files and returns a coverage Collector that contains the remappped
@@ -159,19 +57,15 @@ function getMapping(sourceMap, sourceMapDir, location, useAbsolutePaths, inlineS
  *                              warn?     - a function that logs warnings
  * @return {istanbul/lib/_collector}         The remapped collector
  */
-module.exports = function remap(coverage, options = {}) {
+function remap(coverage, options = {}) {
   const warn = options.warn || console.warn;
 
   let exclude;
   if (options.exclude) {
     if (typeof options.exclude === 'string') {
-      exclude = function (fileName) {
-        return fileName.indexOf(options.exclude) > -1;
-      };
+      exclude = (fileName) => fileName.indexOf(options.exclude) > -1;
     } else {
-      exclude = function (fileName) {
-        return fileName.match(options.exclude);
-      };
+      exclude = (fileName) => fileName.match(options.exclude);
     }
   }
 
@@ -197,11 +91,48 @@ module.exports = function remap(coverage, options = {}) {
       return fs.readFileSync(filePath);
     };
 
-  let srcCoverage = {};
 
   if (!Array.isArray(coverage)) {
     coverage = [coverage];
   }
+
+  let srcCoverage = {};
+
+  const metaInfo = new MaybeWeakMap();
+
+  /**
+   * Generate a coverage object that will be filled with the remapped data
+   * @param  {string} filename    The name of the file that is being remapped
+   * @return {Object}             An object that provides the actual data and
+   *                              its shadow data used for reference.
+   */
+  const getSourceCoverage = (filename) => {
+    let data = srcCoverage[filename];
+    if (!data) {
+      data = srcCoverage[filename] = {
+        path: filename,
+        statementMap: {},
+        fnMap: {},
+        branchMap: {},
+        s: {},
+        b: {},
+        f: {},
+      };
+      metaInfo.set(data, {
+        indexes: {},
+        lastIndex: {
+          s: 0,
+          b: 0,
+          f: 0,
+        },
+      });
+    }
+
+    return {
+      data,
+      meta: metaInfo.get(data),
+    };
+  };
 
   coverage.forEach((item) => {
     Object.keys(item).forEach((filePath) => {
@@ -209,6 +140,7 @@ module.exports = function remap(coverage, options = {}) {
         warn(`Excluding: "${filePath}"`);
         return;
       }
+
       const fileCoverage = item[filePath];
       /* coverage.json can sometimes include the code inline */
       let codeIsArray = true;
@@ -286,129 +218,32 @@ module.exports = function remap(coverage, options = {}) {
 
         sourceMap.sourcesContent.forEach((source, idx) => {
           inlineSourceMap[sourceMap.sources[idx]] = true;
-          getSourceCoverage(srcCoverage, sourceMap.sources[idx]).data.code = codeIsArray ? source.split('\n') : source;
+          getSourceCoverage(sourceMap.sources[idx]).data.code = codeIsArray ? source.split('\n') : source;
           if (sourceStore) {
             sourceStore.set(sourceMap.sources[idx], source);
           }
         });
       }
 
-      Object.keys(fileCoverage.fnMap).forEach((index) => {
-        const genItem = fileCoverage.fnMap[index];
-        const mapping = getMapping(sourceMap, sourceMapDir, genItem.loc, useAbsolutePaths, inlineSourceMap);
-
-        if (!mapping) {
-          return;
+      function resolvePath(source) {
+        let resolvedSource = source in inlineSourceMap
+          ? source
+          : path.resolve(sourceMapDir, source);
+        if (!useAbsolutePaths && !(source in inlineSourceMap)) {
+          resolvedSource = path.relative(process.cwd(), resolvedSource);
         }
+        return resolvedSource;
+      }
 
-        const hits = fileCoverage.f[index];
-        const covInfo = getSourceCoverage(srcCoverage, mapping.source);
-        const data = covInfo.data;
-        const meta = covInfo.meta;
-        const srcItem = {
-          name: genItem.name,
-          line: mapping.loc.start.line,
-          loc: mapping.loc,
-        };
-        if (genItem.skip) {
-          srcItem.skip = genItem.skip;
-        }
-        const key = [
-          'f',
-          srcItem.loc.start.line, srcItem.loc.start.column,
-          srcItem.loc.end.line, srcItem.loc.end.column,
-        ].join(':');
+      const getMappingResolved = (location) => {
+        const mapping = getMapping(sourceMap, location);
+        if (!mapping) return null;
+        return Object.assign(mapping, { source: resolvePath(mapping.source) });
+      };
 
-        let fnIndex = meta.indexes[key];
-        if (!fnIndex) {
-          fnIndex = ++meta.lastIndex.f;
-          meta.indexes[key] = fnIndex;
-          data.fnMap[fnIndex] = srcItem;
-        }
-        data.f[fnIndex] = data.f[fnIndex] || 0;
-        data.f[fnIndex] += hits;
-      });
-
-      Object.keys(fileCoverage.statementMap).forEach((index) => {
-        const genItem = fileCoverage.statementMap[index];
-
-        const mapping = getMapping(sourceMap, sourceMapDir, genItem, useAbsolutePaths, inlineSourceMap);
-
-        if (!mapping) {
-          return;
-        }
-
-        const hits = fileCoverage.s[index];
-        const covInfo = getSourceCoverage(srcCoverage, mapping.source);
-        const data = covInfo.data;
-        const meta = covInfo.meta;
-
-        const key = [
-          's',
-          mapping.loc.start.line, mapping.loc.start.column,
-          mapping.loc.end.line, mapping.loc.end.column,
-        ].join(':');
-
-        let stIndex = meta.indexes[key];
-        if (!stIndex) {
-          stIndex = ++meta.lastIndex.s;
-          meta.indexes[key] = stIndex;
-          data.statementMap[stIndex] = mapping.loc;
-        }
-        data.s[stIndex] = data.s[stIndex] || 0;
-        data.s[stIndex] += hits;
-      });
-
-      Object.keys(fileCoverage.branchMap).forEach((index) => {
-        const genItem = fileCoverage.branchMap[index];
-        const locations = [];
-        let source;
-        let key = ['b'];
-
-        for (let i = 0; i < genItem.locations.length; ++i) {
-          const mapping = getMapping(sourceMap, sourceMapDir, genItem.locations[i], useAbsolutePaths, inlineSourceMap);
-          if (!mapping) {
-            return;
-          }
-          /* istanbul ignore else: edge case too hard to test for */
-          if (!source) {
-            source = mapping.source;
-          } else if (source !== mapping.source) {
-            return;
-          }
-          locations.push(mapping.loc);
-          key.push(
-            mapping.loc.start.line, mapping.loc.start.column,
-            mapping.loc.end.line, mapping.loc.end.line
-          );
-        }
-
-        key = key.join(':');
-
-        const hits = fileCoverage.b[index];
-        const covInfo = getSourceCoverage(srcCoverage, source);
-        const data = covInfo.data;
-        const meta = covInfo.meta;
-
-        let brIndex = meta.indexes[key];
-        if (!brIndex) {
-          brIndex = ++meta.lastIndex.b;
-          meta.indexes[key] = brIndex;
-          data.branchMap[brIndex] = {
-            line: locations[0].start.line,
-            type: genItem.type,
-            locations,
-          };
-        }
-
-        if (!data.b[brIndex]) {
-          data.b[brIndex] = locations.map(() => 0);
-        }
-
-        for (let i = 0; i < hits.length; ++i) {
-          data.b[brIndex][i] += hits[i];
-        }
-      });
+      updateFunctions(fileCoverage, getMappingResolved, getSourceCoverage);
+      updateStatements(fileCoverage, getMappingResolved, getSourceCoverage);
+      updateBranches(fileCoverage, getMappingResolved, getSourceCoverage);
 
       if (sourceMap.sourcesContent && options.basePath) {
         // Convert path to use base path option
@@ -444,4 +279,6 @@ module.exports = function remap(coverage, options = {}) {
   collector.getFinalCoverage();
 
   return collector;
-};
+}
+
+module.exports = remap;