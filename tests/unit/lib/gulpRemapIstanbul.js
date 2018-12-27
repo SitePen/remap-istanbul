fcfe0009209b49c1c43a816c3c963273f034985e
Mangala Sadhu Sangeet Singh Khalsa <mssskhalsa@gmail.com>
2018-12-26 18:06:39 -0700

    Upgrade to Gulp 4 (#181)

--- tests/unit/lib/gulpRemapIstanbul.js
+++ tests/unit/lib/gulpRemapIstanbul.js
@@ -8,23 +8,27 @@ const assert = intern.getPlugin('chai').assert;
 registerSuite('lib/gulpRemapIstanbul', {
 	'remap coverage': function () {
 		var dfd = this.async();
-		gulp.task('remap-istanbul', function () {
+		var runTasks;
+
+		function remapTask () {
 			return gulp.src('tests/unit/support/coverage.json')
 				.pipe(remapIstanbul())
 				.pipe(gulp.dest('tmp/gulp'));
-		});
+		}
 
-		gulp.task('assertions', [ 'remap-istanbul' ], dfd.callback(function () {
+		gulp.task('assertions', dfd.callback(function () {
 			assert.isTrue(fs.existsSync('tmp/gulp/coverage.json'));
 		}));
 
-		gulp.start('assertions');
+		runTasks = gulp.series(remapTask, 'assertions');
+		runTasks();
 	},
 
 	'write reports': function () {
 		var dfd = this.async();
+		var runTasks;
 
-		gulp.task('remap-istanbul', function () {
+		function remapTask () {
 			return gulp.src('tests/unit/support/coverage.json')
 				.pipe(remapIstanbul({
 					reports: {
@@ -33,83 +37,110 @@ registerSuite('lib/gulpRemapIstanbul', {
 						'json': 'tmp/gulp/gulp-coverage.json'
 					}
 				}));
-		});
+		}
 
-		gulp.task('assertions', [ 'remap-istanbul' ], dfd.callback(function () {
+		gulp.task('assertions', dfd.callback(function () {
 			assert.isTrue(fs.existsSync('tmp/gulp/gulp-coverage.json'));
 			assert.isTrue(fs.existsSync('tmp/gulp/lcov.info'));
 			assert.isTrue(fs.existsSync('tmp/gulp/html-report'));
 		}));
 
-		gulp.start('assertions');
+		runTasks = gulp.series(remapTask, 'assertions');
+		runTasks();
 	},
 
 	'html report with inline sources': function () {
 		var dfd = this.async();
+		var runTasks;
 
-		gulp.task('remap-istanbul', function () {
+		function remapTask () {
 			return gulp.src('tests/unit/support/coverage-inlinesource.json')
 				.pipe(remapIstanbul({
 					reports: {
 						'html': 'tmp/gulp/html-report-inline'
 					}
 				}));
-		});
+		}
 
-		gulp.task('assertions', [ 'remap-istanbul' ], dfd.callback(function () {
+		gulp.task('assertions', dfd.callback(function () {
 			assert.isTrue(fs.existsSync('tmp/gulp/html-report-inline'));
 		}));
 
-		gulp.start('assertions');
+		runTasks = gulp.series(remapTask, 'assertions');
+		runTasks();
 	},
 
 	'non-transpiled coverage': {
 		'warn': function () {
 			var dfd = this.async();
+			var runTasks;
 
-			gulp.task('remap-istanbul', function () {
+			function remapTask () {
 				return gulp.src('tests/unit/support/coverage-import.json')
 					.pipe(remapIstanbul({
 						reports: {
 							'html': 'tmp/gulp/html-report-import'
 						}
 					}));
-			});
+			}
 
-			gulp.task('assertions', [ 'remap-istanbul' ], dfd.callback(function () {
+			gulp.task('assertions', dfd.callback(function () {
 				assert.isTrue(fs.existsSync('tmp/gulp/html-report-import'));
 			}));
 
-			gulp.start('assertions');
+			runTasks = gulp.series(remapTask, 'assertions');
+			runTasks();
 		},
 
 		'fail': function () {
 			var dfd = this.async();
-
+			var runTasks;
 			var errorStack = [];
 
 			function trapError(error) {
-				errorStack.push(error);
+				if (error.message.startsWith('Could not find source map')) {
+					errorStack.push(error);
+				}
 				this.emit('end');
 			}
 
-			gulp.task('remap-istanbul', function () {
-				return gulp.src('tests/unit/support/coverage-import.json')
-					.pipe(remapIstanbul({
-						fail: true,
-						reports: {
-							'html': 'tmp/gulp/html-report-fail'
-						}
-					}))
-					.on('error', trapError);
-			});
+			function remapTask () {
+				/*	gulp.series() aborts if any task throws an error. This causes the 'assertions' task to not run
+					if the remapTask has an error, so we wrap the error-emitting stream in a Promise that always
+					resolves. */
+				return new Promise(function (resolve) {
+					var isResolved = false;
 
-			gulp.task('assertions', ['remap-istanbul'], dfd.callback(function () {
-				assert.strictEqual(errorStack.length, 1, 'One error should have been logged');
+					gulp.src('tests/unit/support/coverage-import.json')
+						.pipe(remapIstanbul({
+							fail: true,
+							reports: {
+								'html': 'tmp/gulp/html-report-fail'
+							}
+						}))
+						.on('error', trapError)
+						.on('finish', function () {
+							if (!isResolved) {
+								isResolved = true;
+								resolve();
+							}
+						})
+						.on('end', function () {
+							if (!isResolved) {
+								isResolved = true;
+								resolve();
+							}
+						});
+				});
+			}
+
+			gulp.task('assertions', dfd.callback(function () {
+				assert.strictEqual(errorStack.length, 1, 'Source map error should have been logged');
 				assert.instanceOf(errorStack[0], Error, 'should be of type error');
 			}));
 
-			gulp.start('assertions');
+			runTasks = gulp.series(remapTask, 'assertions');
+			runTasks();
 		}
 	}
 });
